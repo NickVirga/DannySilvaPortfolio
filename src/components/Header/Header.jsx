01e14b4f90093e6701bd39001481866b65a65023
Nick Virga <nsvirga@gmail.com>
2024-02-28 03:36:19 -0500

    home page layout completed, social media icon assets imported and added to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import "./Header.sass"
+import "./Header.sass";
+
+import linkedInIcon from "../../assets/icons/linkedin-icon.png"
+import twitterIcon from "../../assets/icons/twitter-icon.png"
+import instagramIcon from "../../assets/icons/instagram-icon.png"
 
 function Header() {
   return (
     <header className="header">
-      
-      <Link className="header__logo" to="/"><h1>Danny Silva</h1></Link>
+      <Link className="header__logo" to="/">
+        <h1>Danny Silva</h1>
+      </Link>
       <h2>Layout Artist & Background Painter</h2>
       <div>
         <nav className="header__nav">
           <ul className="header__list">
             <li className="header__item">
-              <Link className="header__link" to="/bgpaint">Layout & Paint</Link>
+              <Link className="header__link" to="/bgpaint">
+                Layout & Paint
+              </Link>
             </li>
-            <li className="header__item">
+            <li className="header__item">Professional Work</li>
+            {/* <li className="header__item">
               <Link className="header__link" to="/bluescluesandyou">Blues Clues and You</Link>
             </li>
             <li className="header__item">
@@ -23,26 +31,53 @@ function Header() {
             </li>
             <li className="header__item">
               <Link className="header__link" to="/openseason">Open Season: Call of Nature (2023)</Link>
+            </li> */}
+            <li className="header__item">
+              <Link className="header__link" to="/thesis-em-memoria">
+                Em Memoria
+              </Link>
             </li>
             <li className="header__item">
-              <Link className="header__link" to="/thesis-em-memoria">Em Memoria</Link>
+              <Link className="header__link" to="/pizza-panic-group-film">
+                Pizza Panic!
+              </Link>
             </li>
             <li className="header__item">
-              <Link className="header__link" to="/pizza-panic-group-film">Pizza Panic!</Link>
-            </li>
-            <li className="header__item">
-              <a className="header__link" href="https://www.instagram.com/_okdanny/" target="_blank">Personal Work</a>
+              <a
+                className="header__link"
+                href="https://www.instagram.com/_okdanny/"
+                target="_blank"
+              >
+                Personal Work
+              </a>
             </li>
             {/* <li className="header__item">
               <Link className="header__link" to="/design">Design</Link>
             </li> */}
             <li className="header__item">
-              <Link className="header__link" to="/resume">Resume</Link>
+              <Link className="header__link" to="/resume">
+                Resume
+              </Link>
             </li>
             <li className="header__item">
-              <Link className="header__link" to="/">About</Link>
+              <Link className="header__link" to="/">
+                About
+              </Link>
             </li>
-
+            <li className="header__item">
+              <a className="about__resume-link" href="mailto:dannysilvaart@gmail.com">
+                CONTACT
+              </a>
+            </li>
+            <li className="header__item">
+              <img src={linkedInIcon} />
+            </li>
+            <li className="header__item">
+              <img src={twitterIcon} />
+            </li>
+            <li className="header__item">
+              <img src={instagramIcon} />
+            </li>
           </ul>
         </nav>
       </div>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,19 +8,23 @@ import "./HomePage.sass";
 function HomePage() {
   return (
     <section className="about">
-      <img className="about__profile-pic" src={"/images/AVATAR.png"}></img>
+      <div className="about__img-container">
+        <img className="about__profile-pic" src={"/images/AVATAR.png"}></img>
+      </div>
       <div className="about__content">
-        <h1>ABOUT</h1>
+        <h1 className="about__title">ABOUT</h1>
         <p className="about__bio">
           Danny is an experienced background painter and layout artist working
           in animation. Currently, they are working with 9Story/Brown Bag Films
-          on Sony Pictures Animation's Open Season: Call of Nature as a
+          on Sony Pictures Animation's <i>Open Season: Call of Nature</i> as a
           background artist and previously worked in the same role on Blues
           Clues and You. With experience working on 2D, 3D, and live-action
           productions, they would make an excellent addition to any production
-          team.
+          team. 
         </p>
-        <Link className="about__resume-link" to="/resume">Resume</Link>
+        <Link className="about__resume-link" to="/resume">
+          RESUME
+        </Link>
       </div>
     </section>
   );
