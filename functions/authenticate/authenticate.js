e39477a58ae60101685a4f7a4953670f7c510ab1
Nick Virga <nsvirga@gmail.com>
2024-03-10 08:56:39 -0400

    authenticate function fixed, and protectImage function added

--- functions/authenticate/authenticate.js
+++ functions/authenticate/authenticate.js
@@ -1,26 +1,37 @@
 const jwt = require("jsonwebtoken")
 
 exports.handler = async (event, context) => {
-
+  
   try {
-    const token = jwt.sign(
-      {
-        password: "password"
-      },
-      "e4744410f340cf3f2023dd61ede33b46256632a92536f5df684a908680e7fc7c",
-      {
-        expiresIn: "24h",
-      }
-    );
+    const dataBody = JSON.parse(event.body)
+    const password = JSON.parse(dataBody.body).password
+    
+    if (password === Netlify.env.get("PASSWORD")) {
+      const token = jwt.sign(
+        {
+          password: password
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+          expiresIn: "24h",
+        }
+      );
     
     return {
       statusCode: 200,
-      body: JSON.stringify({token: token}),
+      body: JSON.stringify({ authenticated: true, token: token}),
     };
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ authenticated: false}),
+    };
+  }
   } catch (error) {
     return {
-      statusCode: 500,
-      body: JSON.stringify(error),
+      statusCode: 400,
+      body: JSON.stringify({authenticated: true, error: error}),
     };
   }
 };
+
