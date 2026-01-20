c737569817d1ce92f1c008c287d3804263200be0
NickVirga <nsvirga@gmail.com>
2026-01-20 15:21:12 -0500

    updated resume link, bio pronouns, x link removed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Hero from "../../components/Hero/Hero"
 
 import "./HomePage.sass";
@@ -16,11 +15,11 @@ function HomePage() {
         <h2 className="about__title">ABOUT</h2>
         <p className="about__bio">
           Danny is an experienced illustrator and background artist working
-          in animation. They have most recently worked with 9Story/Brown Bag Films
+          in animation. He has most recently worked with 9Story/Brown Bag Films
           on Sony Pictures Animation's <i>Open Season: Call of Nature</i> as a
           background artist and previously worked in the same role on Nick Jr.'s <i>Blues
           Clues and You</i>. With experience working on 2D, 3D, and live-action
-          productions, they would make an excellent addition to any production
+          productions, he would make an excellent addition to any production
           team. 
         </p>
         <a className="about__resume-link" href="https://drive.google.com/file/d/14_w0y63uVIi85OGG6Yx8Gef0n6952Ll_/view?usp=sharing" target="_blank" aria-label="RESUME">
